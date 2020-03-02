bd3353a31b632428c724a1acc10b700e3beccd14
fudasatomi <imotas.aduf@gmail.com>
2020-03-02 16:21:52 +0900

    favorite redirect_to

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root 'home#top'
   get 'home/about'
   devise_for :users
-  resources :books
+  resources :books do
+  	resource :favorites, only:[:create,:destroy]
+  end
   resources :users,only: [:show,:index,:edit,:update]
 end
