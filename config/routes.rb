04d6462d94713d4039a4c4867475d0205326d70f
fudasatomi <imotas.aduf@gmail.com>
2020-03-03 16:35:55 +0900

    book_comments destroy corrected

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   root 'home#top'
   get 'home/about'
   devise_for :users
+    resources :users,only: [:show,:index,:edit,:update]
+
   resources :books do
   	resource :favorites, only:[:create,:destroy]
-  	resource :book_comments, only:[:create]
+  	resource :book_comments, only:[:create, :destroy]
   end
-  resources :book_comments,only:[:destroy]
-  resources :users,only: [:show,:index,:edit,:update]
+
 end
